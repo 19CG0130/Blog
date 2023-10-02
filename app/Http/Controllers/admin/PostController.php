b68f199b08225580bb9afeda15c00ddbea0761b8
Emiliano <71781378+19CG0130@users.noreply.github.com>
2023-10-02 16:46:20 -0600

    :)

--- app/Http/Controllers/admin/PostController.php
+++ app/Http/Controllers/admin/PostController.php
@@ -5,13 +5,52 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+use App\Models\Category;
 
 class PostController extends Controller
 {
     public function index() {
+        $datos = Post::select(['posts.*','categorys.category'])
+            ->join('categorys','posts.id_category','=','categorys.id')
+            ->where('posts.id_user','=',Auth::user()->id)
+            ->get();
+            $c = Category::all();
 
-        $datos = Post::all();
         return view('admin.posts')
-            ->with('registros', $datos);
+            ->with('registros', $datos)
+            ->with('categorys',$c);
     }
+    public function store(Request $request) {
+        $reglas = Validator::make($request->all(),[
+            'title'=>'required|min:5|max:255',
+            'content'=>'required',
+            'img'=>'required|mimes:jpg,png,gif,wep|size:2048',
+        ]);
+        if($reglas->fails()){
+            return back()
+            ->withInput()
+            ->with('errorInsertar','Favor de llenar todos los campos')
+            ->withErrors($reglas);
+        }else{
+            $usuario = Post::create([
+                'title'=>$request->title,
+                'content'=>$request->content,
+                'img'=>'default.jpg',
+                'slug'=>'',
+                'likes'=>0,
+                'id_user'=>Auth::user()->id,
+            ]);
+            //parametros del nickname
+            $usuario->nickname = 'user_'.$usuario->id;
+            $usuario->save();
+            
+            return back()
+            ->with('success','Datos insertados correctamente');
+            //dd("DATO INSERTADO");
+            //dd("CUMPLE");
+        }
+        //dd($request);
+    }//llave store
 }
--- resources/views/admin/posts.blade.php
+++ resources/views/admin/posts.blade.php
@@ -2,26 +2,26 @@
 @section('contenedor')
     <div class="container">
         <div class="d-flex justify-content-between py-4">
-                    <h1>Mis Publicaciones</h1>
+            <h1>Mis Publicaciones</h1>
             <button type="button" class="btn btn-outline-succes" data-toggle="modal" data-target="#modalAdd">
                 <i class="fa fa-plus"></i>
             </button>
         </div>
 
-        @if($mensaje=Session::get('errorInsertar'))
+        @if ($mensaje = Session::get('errorInsertar'))
             <div class="alert alert-danger">
-                <h5>Error: {{$mensaje}}</h5>
+                <h5>Error: {{ $mensaje }}</h5>
                 <ul>
-                    @foreach($errors->all() as $error )
-                        <li>{{$error}}</li>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
                     @endforeach
                 </ul>
             </div>
         @endif
 
-        @if($mensaje=Session::get('success'))
+        @if ($mensaje = Session::get('success'))
             <div class="alert alert-success">
-                <h5>Listo: {{$mensaje}}</h5>
+                <h5>Listo: {{ $mensaje }}</h5>
             </div>
         @endif
 
@@ -36,12 +36,12 @@
                     <th></th>
                 </thead>
                 <tbody>
-                    @foreach($registros as $d)
+                    @foreach ($registros as $d)
                         <tr>
                             <td>{{ $d->id }}</td>
-                            <td>{{ $d->title}}</td>
+                            <td>{{ $d->title }}</td>
                             <td>{{ $d->likes }}</td>
-                            <td>{{ $d->id_category }}</td>
+                            <td>{{ $d->category }}</td>
                             <td>{{ $d->created_at }}</td>
                             <td>
                                 <button class="btn btn-outline-danger">
@@ -57,49 +57,52 @@
             </table>
         </div>
     </div>
-<!-- Modal -->
-<div class="modal fade" id="modalAdd" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle">Agregar Usuario</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+    <!-- Modal -->
+    <div class="modal fade" id="modalAdd" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Agregar Entrada</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <form action="/admin/posts" method="POST">
+                    @csrf
+                    <div class="modal-body">
+
+                        <div class="form-group">
+                            <label for="">Titulo:</label>
+                            <input type="text" class="form-control" name="title">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="">Descripcion:</label>
+                            <textarea name="content" id="" cols="30" rows="10" class="form-control"></textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="">Imagen:</label>
+                            <input type="file" class="form-control" name="img">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Categoría</label>
+                            <select name="id_category" class="form-control">
+                                @foreach($categorys as $c)
+                                    <option value="{{ $c->id }}">{{ $c->category }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                    </div>
+                </form>
+
+            </div>
         </div>
-
-        <form action="/admin/users" method="POST">
-            @csrf
-        <div class="modal-body">
-
-            <div class="form-group">
-                <label for="">Nombre:</label>
-                <input type="text" class="form-control" name="name">
-            </div>
-
-            <div class="form-group">
-                <label for="">Email:</label>
-                <input type="email" class="form-control" name="email">
-            </div>
-
-            <div class="form-group">
-                <label for="">Password:</label>
-                <input type="password" class="form-control" name="password">
-            </div>
-
-            <div class="form-group">
-                <label for="">Confirmar password:</label>
-                <input type="password" class="form-control" name="password_confirm">
-            </div>
-
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="submit" class="btn btn-primary">Save changes</button>
-        </div>
-    </form>
-
-      </div>
     </div>
-  </div>
 @endsection
